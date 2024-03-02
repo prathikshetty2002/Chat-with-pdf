a61b1e7805e0fa9478d1da416b80b274a13167d0
prathik shetty <prathikkshetty15@gmail.com>
2024-03-02 21:57:07 +0530

    data polivy

--- app.py
+++ app.py
@@ -19,32 +19,32 @@ def send_message_to_bot(data):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('send_message_form.html')
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    file = request.files['file']
+# @app.route('/upload', methods=['POST'])
+# def upload_file():
+#     file = request.files['file']
     
-    if file.filename == '':
-        return 'No selected file'
+#     if file.filename == '':
+#         return 'No selected file'
     
-    files = {'file': ('file.pdf', file.stream, 'application/octet-stream')}
-    headers = {'x-api-key': 'sec_oixDgaiu41gif64dTAXyYgaZaw3XscVS'}  
-    response = requests.post('https://api.chatpdf.com/v1/sources/add-file', headers=headers, files=files)
+#     files = {'file': ('file.pdf', file.stream, 'application/octet-stream')}
+#     headers = {'x-api-key': 'sec_oixDgaiu41gif64dTAXyYgaZaw3XscVS'}  
+#     response = requests.post('https://api.chatpdf.com/v1/sources/add-file', headers=headers, files=files)
     
-    if response.status_code == 200:
-        source_id = response.json()['sourceId']
-        session['source_id'] = source_id  # Store sourceId in session
-        return redirect('/send-message')  
-    else:
-        return f'Error: {response.status_code} - {response.text}'
+#     if response.status_code == 200:
+#         source_id = response.json()['sourceId']
+#         session['source_id'] = source_id  # Store sourceId in session
+#         return redirect('/send-message')  
+#     else:
+#         return f'Error: {response.status_code} - {response.text}'
 
 @app.route('/send-message', methods=['GET','POST'])
 def send_message():
     if 'source_id' not in session:
         return 'Source ID not found in session'
     
-    source_id = session['source_id']  # Retrieve sourceId from session
+    source_id = 'src_NmoU5zgj6y9eYn8iPxUa8' # Retrieve sourceId from session
     
     if request.method == 'POST':
         user_message = request.form['user_message']
